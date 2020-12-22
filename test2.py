f56e413b7874ded9fc36d2065384b92a4b15a75a
Najat Abdella <nabdella00@gmail.com>
2020-12-21 22:48:13 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,11 +1,13 @@
-from FINAL_PROJECT import print_movies
+from FINAL_PROJECT import __init__, print_movies
 import pytest
 #Najat
-#def test_init():
-   # """" Tests the __init__() method of the MovieDB class."""
-
-#movie =MovieDB()
-
+def test_init():
+   """" Tests the __init__() method of the MovieDB class and has the correct amount of rows"""
+if len(self.movie_db.columns) == 7:
+    pass
+else:
+    exit()
+    
 
 def test_print_movies(capsys):
     """Test the print_movies() method of the MovieDB class."""
