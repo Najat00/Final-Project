8ef560fc51eaa1888dee9abfd9306d1b1f3f9422
Najat Abdella <nabdella00@gmail.com>
2020-12-22 17:40:16 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,5 +1,5 @@
 from FINAL_PROJECT import MovieDB, __init__
-import pytest
+from unittest import mock
 #Najat
 def test_init():
     """" Tests the __init__() method of the MovieDB class and has the 
@@ -13,8 +13,10 @@ else:
 
 def test_print_movies(capsys):
     """Test the print_movies() method of the MovieDB class."""
-    captured= capsys.readouterr()
-    captured.out == "movie_db.\n"
+with mock.patch("builtins.input",
+                    side_effect=["11"]):
+    m = MovieDB("movies.csv")
+    assert m.print_movies() == print(pandas.read_csv("movies.csv"))
     
     
     
