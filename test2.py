b776bc328c7da27c218c60b9c84c273c771d4c21
Najat Abdella <nabdella00@gmail.com>
2020-12-22 16:39:55 -0500

    updates

--- test2.py
+++ test2.py
@@ -1,13 +1,15 @@
-from FINAL_PROJECT import __init__, print_movies
+from FINAL_PROJECT import MovieDB, __init__
 import pytest
 #Najat
 def test_init():
-   """" Tests the __init__() method of the MovieDB class and has the correct amount of rows"""
-if len(self.movie_db.columns) == 7:
+    """" Tests the __init__() method of the MovieDB class and has the 
+correct amount of columns
+""" 
+mdb = MovieDB("movies.csv")
+if len(mdb.movie_db.columns) == 7:
     pass
 else:
     exit()
-    
 
 def test_print_movies(capsys):
     """Test the print_movies() method of the MovieDB class."""
--- test3.py
+++ test3.py
@@ -6,5 +6,8 @@ from unittest import mock
 
 
 def testo(capsys):
+     """ Parameters: n/a
+    Takes in users age and stores that value as an integer in the age variable. 
+    Set conditionals to check if user is below age"""
     with mock.patch('builtins.input', side_effect= ["12"]):
         assert age_verification() == "The age of 12 is to young, you'll need a parents approval to access movies in this database!\n"
