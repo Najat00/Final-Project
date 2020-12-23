a6dd98d4e50c6367ce09926dcb9d2492fa5ba5db
Nikky Mittu <nkmittu31@gmail.com>
2020-12-22 19:20:48 -0500

    Updated test3.py (Dale)

--- test3.py
+++ test3.py
@@ -1,13 +1,11 @@
-#David McCoy and Dale Nche
-
-from fp import age_verification
+from FINAL_PROJECT import age_verification
 import builtins
 from unittest import mock
 
 
 def testo(capsys):
-     """ Parameters: n/a
-    Takes in users age and stores that value as an integer in the age variable. 
-    Set conditionals to check if user is below age"""
+    """ Parameters: n/a Takes in users age and stores that value as an integer in the age variable. 
+    Set conditionals to check if user is below age. 
+    """
     with mock.patch('builtins.input', side_effect= ["12"]):
         assert age_verification() == "The age of 12 is to young, you'll need a parents approval to access movies in this database!\n"
