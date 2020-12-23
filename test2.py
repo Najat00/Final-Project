02f5f6ca040f9e9e64feedf8ea7d1d1e8ebc90fe
Nikky Mittu <nkmittu31@gmail.com>
2020-12-22 19:16:43 -0500

    Updated test2.py file (Najat and Nikky)

--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
 from FINAL_PROJECT import MovieDB, __init__
 from unittest import mock
-#Najat
+import pandas as pd
+
 def test_init():
     """" Tests the __init__() method of the MovieDB class and has the 
 correct amount of columns
@@ -16,7 +17,7 @@ def test_print_movies(capsys):
 with mock.patch("builtins.input",
                     side_effect=["11"]):
     m = MovieDB("movies.csv")
-    assert m.print_movies() == print(pandas.read_csv("movies.csv"))
+    assert m.print_movies() == print(pd.read_csv("movies.csv"))
     
     
     
